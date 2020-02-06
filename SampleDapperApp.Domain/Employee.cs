86c02ec68ea9c26e094d994f081d7bb6b49231dd
Vishal Poduval <vishal.p.poduval@citi.com>
2020-02-06 20:09:40 +0530

    Initial Commit

---
+++ SampleDapperApp.Domain/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SampleDapperApp.Domain
+{
+	public class Employee
+	{
+		public int EmployeeID { get; set; }
+		public string EmployeeName { get; set; }
+	}
+}
---
+++ SampleDapperApp.Domain/EmployeeDepartments.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleDapperApp.Domain
+{
+	public class EmployeeDepartments
+	{
+		public int EmployeeId { get; set; }
+		public List<Department> Departments { get; set; }
+	}
+}
